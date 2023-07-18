43f869f35c9c60ee4f076a7e6e8f71acc603fa98
novbee-bee <110586312+novbee-bee@users.noreply.github.com>
2023-07-18 14:37:05 +0800

    Added GET request for notes list

--- packages/functions/src/get.ts
+++ packages/functions/src/get.ts
@@ -1,27 +1,22 @@
-import {Table} from "sst/node/table";
-import handler from "@notes/core/handler"
-import dynamodb from "@notes/core/dynamodb";
+import { Table } from "sst/node/table";
+import handler from "@notes/core/handler";
+import dynamoDb from "@notes/core/dynamodb";
 
-export const main = handler(async(event: any) => {
+export const main = handler(async (event: any) => {
+  const params = {
+    TableName: Table.Notes.tableName,
+    // 'Key' defines the partition key and sort key of the item to be retrieved
+    Key: {
+      userId: "123", // The id of the author
+      noteId: event.pathParameters.id, // The id of the note from the path
+    },
+  };
 
-    const prams = {
-        TableName: Table.Notes.tableName,
-        // 'Key' defines the partition key and sort key of the item to be retrieved
-        Key: {
-            userId: "123", // The id of the author
-            noteId: event.pathParameters.id, // The id of the note from the path
-        },
-    };
+  const result = await dynamoDb.get(params);
+  if (!result.Item) {
+    throw new Error("Item not found.");
+  }
 
-    const result = await dynamodb.get(prams);
-
-    if(!result.Item){
-        throw new Error("Item not found.");
-    }
-
-    // Return the retrieved item
-    return result.Item;
-
-
-
+  // Return the retrieved item
+  return result.Item;
 });
