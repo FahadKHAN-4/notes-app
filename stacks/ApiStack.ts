a254e6850092ea3e17068a7946f3bb2a3432969b
FahadKHAN-4 <khanfahad0059@gmail.com>
2023-07-19 16:40:53 +0800

    Added stripe for payments

--- stacks/ApiStack.ts
+++ stacks/ApiStack.ts
@@ -11,6 +11,9 @@ export function ApiStack({stack, app}: StackContext){
             authorizer: "iam",
             function:{
                 bind: [table],
+                environment: {
+                    STRIPE_SECRET_KEY: process.env.STRIPE_SECRET_KEY as string,
+                },
             },
         },
 
@@ -20,6 +23,7 @@ export function ApiStack({stack, app}: StackContext){
             "GET /notes": "packages/functions/src/list.main",
             "PUT /notes/{id}": "packages/functions/src/update.main",
             "DELETE /notes/{id}": "packages/functions/src/delete.main",
+            "POST /billing": "packages/functions/src/billing.main",
             
         },
     });
