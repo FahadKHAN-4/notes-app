d0cceb1159882ababa86ba8ff696324818cd1058
FahadKHAN-4 <khanfahad0059@gmail.com>
2023-07-20 11:15:34 +0800

    Enabling CORS

--- packages/core/src/handler.ts
+++ packages/core/src/handler.ts
@@ -13,9 +13,19 @@ export default function handler(lambda : any) {
       }
   
       // Return HTTP response
+      // return {
+      //   statusCode,
+      //   body: JSON.stringify(body),
+      // };
+
       return {
         statusCode,
         body: JSON.stringify(body),
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Credentials": true,
+        },
       };
+      
     };
   }
