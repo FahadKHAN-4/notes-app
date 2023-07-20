9cd96b959256cecf98abea9c77f7d34b5688ed57
FahadKHAN-4 <khanfahad0059@gmail.com>
2023-07-20 12:01:29 +0800

    added front end stack

--- sst.config.ts
+++ sst.config.ts
@@ -2,6 +2,7 @@ import { SSTConfig } from "sst";
 import { StorageStack } from "./stacks/StorageStack";
 import { ApiStack } from "./stacks/ApiStack";
 import { AuthStack } from "./stacks/AuthStack";
+import { FrontendStack } from "./stacks/FrontendStack";
 
 export default {
   config(_input) {
@@ -10,9 +11,18 @@ export default {
       region: "us-east-1",
     };
   },
+  // stacks(app) {
+  //   app.stack(StorageStack).stack(ApiStack).stack(AuthStack);
+  // },
+
   stacks(app) {
-    app.stack(StorageStack).stack(ApiStack).stack(AuthStack);
+    app
+      .stack(StorageStack)
+      .stack(ApiStack)
+      .stack(AuthStack)
+      .stack(FrontendStack);
   },
+  
 } satisfies SSTConfig;
 
 
